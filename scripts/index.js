019d794b9bc0c02d7f2fcee169d76e38e740f211
Alexey Pakhomov <pakhomovs.mail@yandex.ru>
2022-10-29 00:28:13 +0300

    v 1.0

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let openPopup = document.querySelector('.profile__edit-button');
+let closePopup = document.querySelector('.popup__close-icon');
+let popupContainer = document.querySelector('.popup');
+let nameInput = document.querySelector('.popup__input_text_name');
+let jobInput = document.querySelector('.popup__input_text_job');
+let profileName = document.querySelector('.profile__name');
+let profileSpecialization = document.querySelector('.profile__specialization');
+let formElement = document.querySelector('.popup__form');
+let popupButton = document.querySelector('.popup__button');
+
+function togglePopup() {
+  popupContainer.classList.toggle('popup_opened');
+  if (popupContainer.classList.contains('popup_opened')) {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileSpecialization.textContent;
+  } else {
+    nameInput.value = '';
+    jobInput.value = '';
+  };
+};
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileSpecialization.textContent = jobInput.value;
+  togglePopup();
+};
+
+openPopup.addEventListener('click',togglePopup);
+closePopup.addEventListener('click',togglePopup);
+popupContainer.addEventListener('click',function () {
+  if (event.target === event.currentTarget) {
+    popupContainer.classList.toggle('popup_opened');
+  }
+});
+formElement.addEventListener('submit',formSubmitHandler);
